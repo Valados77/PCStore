e4b0e14d9f09dcb009bc7e0218a9fbea74bf487c
Vladislav <vprokopovich2002@gmail.com>
2023-05-10 00:23:10 +0300

    add authentification

--- PCStore_MVC/Controllers/HomeController.cs
+++ PCStore_MVC/Controllers/HomeController.cs
@@ -1,48 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PCStore_MVC.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using PCStore_MVC.Data;
 using PCStore_MVC.Models.ModelDB;
 
 namespace PCStore_MVC.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-		private readonly ApplicationDbContext db;
+        private readonly ApplicationDbContext db;
 
-		public HomeController(ILogger<HomeController> logger,
-			ApplicationDbContext injectedContext)
-		{
-			_logger = logger;
-			db = injectedContext;
-		}
+        public HomeController(ILogger<HomeController> logger,
+            ApplicationDbContext injectedContext)
+        {
+            _logger = logger;
+            db = injectedContext;
+        }
 
-		[ResponseCache(Duration = 10 /* seconds */,
-			Location = ResponseCacheLocation.Any)]
-		public async Task<IActionResult> Index()
-		{
-			HomeIndexViewModel model = new
-			(
-				Users: await db.Users.ToListAsync(),
-				Categories: await db.Categories.ToListAsync(),
-				Products: await db.Products.ToListAsync()
-			);
+        [ResponseCache(Duration = 10 /* seconds */,
+            Location = ResponseCacheLocation.Any)]
+        public async Task<IActionResult> Index()
+        {
+            HomeIndexViewModel model = new
+            (
+                Users: await db.Users.ToListAsync(),
+                Categories: await db.Categories.ToListAsync(),
+                Products: await db.Products.ToListAsync()
+            );
 
-			return View(model); // pass model to view
-		}
+            return View(model); // pass model to view
+        }
 
+		[Route("private")]
+		[Authorize(Roles = "Administrators")]
 		public IActionResult Privacy()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CategoryDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest("You must pass a category ID in the route, for example, /Home/CategoryDetail/21");
+            }
+
+            Category? model = db.Categories.SingleOrDefault(p => p.CategoryId == id);
+            if (model is null)
+            {
+                return NotFound($"CategoryId {id} not found.");
+            }
+
+            return View(model); // pass model to view and then return result
+        }
+    }
 }
---
+++ PCStore_MVC/Controllers/RolesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PCStore_MVC.Models.ModelDB;
+
+namespace PCStore_MVC.Controllers
+{
+	public class RolesController : Controller
+	{
+		private string AdminRole = "Administrators";
+		private string UserEmail = "test@example.com";
+		private readonly RoleManager<IdentityRole> roleManager;
+		private readonly UserManager<ApplicationUser> userManager;
+
+		public RolesController(RoleManager<IdentityRole> roleManager,
+			UserManager<ApplicationUser> userManager)
+		{
+			this.roleManager = roleManager;
+			this.userManager = userManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			if (!(await roleManager.RoleExistsAsync(AdminRole)))
+			{
+				await roleManager.CreateAsync(new IdentityRole(AdminRole));
+			}
+
+			ApplicationUser user = await userManager.FindByEmailAsync(UserEmail);
+			if (user == null)
+			{
+				user = new();
+				user.UserName = UserEmail;
+				user.Email = UserEmail;
+				IdentityResult result = await userManager.CreateAsync(
+					user, "Pa$$w0rd");
+				if (result.Succeeded)
+				{
+					Console.WriteLine($"User {user.UserName} created successfully.");
+				}
+				else
+				{
+					foreach (IdentityError error in result.Errors)
+					{
+						Console.WriteLine(error.Description);
+					}
+				}
+			}
+
+			if (!user.EmailConfirmed)
+			{
+				string token = await userManager
+					.GenerateEmailConfirmationTokenAsync(user);
+				IdentityResult result = await userManager
+					.ConfirmEmailAsync(user, token);
+				if (result.Succeeded)
+				{
+					Console.WriteLine($"User {user.UserName} email confirmed successfully.");
+				}
+				else
+				{
+					foreach (IdentityError error in result.Errors)
+					{
+						Console.WriteLine(error.Description);
+					}
+				}
+			}
+
+			if (!(await userManager.IsInRoleAsync(user, AdminRole)))
+			{
+				IdentityResult result = await userManager.AddToRoleAsync(user,
+					AdminRole);
+				if (result.Succeeded)
+				{
+					Console.WriteLine($"User {user.UserName} added to {AdminRole} successfully.");
+				}
+				else
+				{
+					foreach (IdentityError error in result.Errors)
+					{
+						Console.WriteLine(error.Description);
+					}
+				}
+			}
+
+			return Redirect("/");
+		}
+	}
+}
